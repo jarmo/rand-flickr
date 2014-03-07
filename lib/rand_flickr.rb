5e387cf554d112ae8adc0cfa304298b2ef7aef62
Jarmo Pertman <jarmo.p@gmail.com>
2014-03-07 23:30:20 +0200

    Ajaxify next image loading.

--- lib/rand_flickr.rb
+++ lib/rand_flickr.rb
@@ -34,11 +34,19 @@ class RandFlickr < Sinatra::Base
     call env.merge("PATH_INFO" => "/photo/#{params[:user]}")
   end
 
+  get "/photo/:user.json" do
+    username = params[:user]
+    photo = random_photo username 
+    photo.merge!(browser_url: browser_url(username, photo))
+
+    content_type :json
+    MultiJson.dump photo
+  end
+
   get "/photo/:user" do
     username = params[:user]
-    photo = FlickrApi.new(ENV["FLICKR_API_KEY"], username).random_photo
-    session[:photo] = photo
-    redirect to("/photo/#{username}/#{photo[:photoset][:id]}/#{photo[:id]}")
+    session[:photo] = photo = random_photo username
+    redirect to(browser_url(username, photo))
   end
 
   get "/photo/:user/:photoset_id/:photo_id" do
@@ -49,6 +57,16 @@ class RandFlickr < Sinatra::Base
 
   get "/style.css" do
     scss :style
+  end
+
+  private
+
+  def random_photo(username)
+    FlickrApi.new(ENV["FLICKR_API_KEY"], username).random_photo
+  end
+
+  def browser_url(username, photo)
+    "/photo/#{username}/#{photo[:photoset][:id]}/#{photo[:id]}"
   end
 
   run! if app_file == $0
--- public/script.js
+++ public/script.js
@@ -1,16 +1,60 @@
 var PhotoRenderer = function(container, photo) {
+  bindEvents();
+
   this.render = function() {
-    NProgress.configure({minimum: 0.3}).start();
+    startProgress()
+    renderImage(photo)
+
+    return this
+  }
+
+  function bindEvents() {
+    $("a").click(renderNewImage)
+
+    $(window).on("popstate", renderImageFromHistory)
+  }
+
+  function renderImage(photo) {
+    var dfd = $.Deferred()
 
     $("<img>").load(function() {
       $(this).remove()
+
       container
         .css("background-image", "url(" + photo.url + ")")
         .find(".caption").html(photo.photoset.title._content + "<br>" + photo.title + "<br>" + photo.ownername).end()
         .fadeIn()
-      NProgress.done();
+
+      NProgress.done()
+      dfd.resolve()
     }).attr("src", photo.url)
 
-    return this
+    return dfd
+  }
+
+  function renderNewImage(ev) {
+    ev.preventDefault()
+
+    startProgress()
+
+    var photoJsonUrl = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + $("a").attr("href") + ".json"
+    $.getJSON(photoJsonUrl).done(replaceCurrentImage)
+  }
+
+  function renderImageFromHistory() {
+    if (!event.state) return
+
+    startProgress()
+    renderImage(event.state)
+  }
+
+  function replaceCurrentImage(photo) {
+    renderImage(photo).done(function() {
+      history.pushState(photo, "", photo.browser_url)      
+    })
+  }
+
+  function startProgress() {
+    NProgress.configure({minimum: 0.3}).start()    
   }
 }
